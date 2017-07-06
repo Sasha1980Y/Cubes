aff34780b091d79b49421c3cae427d73bb8e7d04
AlexanderYakovenko <AlexanderYakovenko@Hellens-Mac-mini.local>
2017-07-06 11:56:31 +0300

    all work !!!

--- Cubes/CubesView.swift
+++ Cubes/CubesView.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class CubesView: UIView {
 
+    var horConstraint: NSLayoutConstraint?
+    var verConstraint: NSLayoutConstraint?
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        //self.frame = CGRect(x: 20, y: 220, width: 50, height: 50)
         frameMyCub()
     }
     
@@ -27,72 +29,7 @@ class CubesView: UIView {
         self.backgroundColor = getRandomColor()
         self.translatesAutoresizingMaskIntoConstraints = false
         
-        
-        
-        /*
-        let horConst = NSLayoutConstraint(item: self,
-                                          attribute: .centerX,
-                                          relatedBy: .equal,
-                                          toItem: superview ,
-                                          attribute: .centerX,
-                                          multiplier: 1,
-                                          constant: 0.0)
-        let verConst = NSLayoutConstraint(item: self,
-                                          attribute: .top,
-                                          relatedBy: .equal,
-                                          toItem: superview,
-                                          attribute: .top,
-                                          multiplier: 1.0,
-                                          constant: -superview!.frame.size.height * 0.1    )
-        let widConst = NSLayoutConstraint(item: self,
-                                          attribute: .width,
-                                          relatedBy: .equal,
-                                          toItem: superview ,
-                                          attribute: .width,
-                                          multiplier: 0.2,
-                                          constant: 0.0)
-        let heiConst = NSLayoutConstraint(item: self,
-                                          attribute: .height,
-                                          relatedBy: .equal,
-                                          toItem: superview ,
-                                          attribute: .height,
-                                          multiplier: 0.1,
-                                          constant: 0.0)
-        
-        superview!.addConstraints([horConst, verConst, widConst, heiConst])
-        */
-        
-        /*
-        let timer = Timer.scheduledTimer(withTimeInterval: 3.0, repeats: false) {_ in
-            
-            UIView.animate(withDuration: 5.0, delay: 2.0, animations: {
-                const.constant = (self.superview!.frame.size.height)
-                self.superview!.layoutIfNeeded()
-            }, completion: {_ in
-                
-                //self.removeFromSuperview()
-            })
-            
-        }
-        RunLoop.current.add(timer, forMode: .commonModes)
-        
-        */
-        
-        
-        
-        
-
     }
-    
-    override func didMoveToSuperview() {
-        super.didMoveToSuperview()
-        if superview != nil {
-            
-            
-            
-        }
-    }
-    
     
     func getRandomColor() -> UIColor {
         let randomRed: CGFloat = CGFloat(drand48())
--- Cubes/ViewController.swift
+++ Cubes/ViewController.swift
@@ -10,35 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var countOfAll = 0
+    var countOfCubDidDown = 0
     var countOfTaps = 0
-    var countOfCubes = 3
-    
+    var countOfCubes = 2
     
     
     var arrayOfCubes = [CubesView]()
    
     
     @IBOutlet weak var countOfTapLabel: UILabel!
-    @IBOutlet weak var cuontOfAllLabel: UILabel!
+    
+    @IBOutlet weak var countOfCubDidDownLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        /*
-        let constWidthView = self.view.frame.size.width
-        print(constWidthView)
-        let constHeigthView = self.view.frame.size.height
-        print(constHeigthView)
-        let constWidthCub = Int(constWidthView * 0.177)
-        print(constWidthCub)
-        let constHeigthCub = Int(constHeigthView * 0.1)
-        print(constHeigthCub)
-        */
-        
-        
-        
-
         
         
     }
@@ -46,23 +32,16 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         
         
-        
-        
-            
         countOfTapLabel.text = String(countOfTaps)
         
-        for item in 0..<countOfCubes {
-            let newCub = CubesView()
-            arrayOfCubes.append(newCub)
-            view.addSubview(newCub)
-            addNewCub(sender: arrayOfCubes[item], superview: self.view)
-            
-            
+        for _ in 0..<countOfCubes {
+        let newCub = CubesView()
+        arrayOfCubes.append(newCub)
+        view.addSubview(newCub)
+           addConstr(sender: newCub , superview: self.view)
+           addAnimation(sender: newCub, superview: self.view)
+        
         }
-        
-        
-        
-        
         
     }
     
@@ -73,41 +52,23 @@ class ViewController: UIViewController {
         let touch = touches.first
         let touchLocation = touch?.location(in: self.view)
         
-        
-        print("subview \(self.view.subviews)")
-        print("end")
-        
         for view in (self.view.subviews).reversed() {
             
-        
             
-        
             if let _ = view.layer.presentation()?.hitTest(touchLocation!)  /*, view.tag == 999*/ {
                 
-                if view.layer != countOfTapLabel.layer && view.layer != cuontOfAllLabel.layer   {
-                    print("Ok")
-                    countOfTaps += 1
+                if view.layer != countOfTapLabel.layer && view.layer != countOfCubDidDownLabel.layer   {
+                                        countOfTaps += 1
                     countOfTapLabel.text = String(countOfTaps)
                 
-                    
-                    
-                        for (key, value) in arrayOfCubes.enumerated() {
+                    for (_ , value) in arrayOfCubes.enumerated() {
                             if view.layer == value.layer {
-                                print("create new cub")
-                            print(value.frame)
                                 
-                                addNewCub(sender: value, superview: self.view)
-                                print("*****")
-                                print(key)
-                                print("*****")
-                            
+                                addAnimation(sender: value, superview: self.view)
+                                value.layer.removeAllAnimations()
                             }
+                    }
                     
-
-                        }
-                                        
-                
-                //view.removeFromSuperview()
                 return
                 }
                 
@@ -115,97 +76,79 @@ class ViewController: UIViewController {
             
         }
         
-        
-        
-        
-        
     
     }
     
-    
-    func addNewCub(sender: UIView, superview: UIView) {
-        let horConstr = NSLayoutConstraint(item: sender,
+    func addConstr(sender: CubesView, superview: UIView) {
+        
+        sender.horConstraint = NSLayoutConstraint(item: sender,
                                            attribute: .left,
                                            relatedBy: .equal ,
                                            toItem: superview,
                                            attribute: .left,
                                            multiplier: 1.0,
                                            constant: CGFloat(arc4random_uniform(UInt32(superview.frame.size.width)-UInt32(superview.frame.size.height * 0.1))))
-        let verConstr = NSLayoutConstraint(item: sender,
+        sender.verConstraint = NSLayoutConstraint(item: sender,
                                            attribute: .top,
                                            relatedBy: .equal,
                                            toItem: superview,
                                            attribute: .top,
                                            multiplier: 1.0,
                                            constant: superview.frame.size.height * 0.1)
-        let widConstr = NSLayoutConstraint(item: sender, attribute: .width, relatedBy: .equal, toItem: superview, attribute: .width, multiplier: 0.2, constant: 0.0)
-        let heiConstr = NSLayoutConstraint(item: sender, attribute: .height, relatedBy: .equal, toItem: superview, attribute: .height, multiplier: 0.1, constant: 0.0)
-        superview.addConstraints([horConstr, verConstr, widConstr, heiConstr])
+        let widConstr = NSLayoutConstraint(item: sender,
+                                           attribute: .width,
+                                           relatedBy: .equal,
+                                           toItem: superview,
+                                           attribute: .width,
+                                           multiplier: 0.2,
+                                           constant: 0.0)
+        let heiConstr = NSLayoutConstraint(item: sender,
+                                           attribute: .height,
+                                           relatedBy: .equal,
+                                           toItem: superview,
+                                           attribute: .height,
+                                           multiplier: 0.1,
+                                           constant: 0.0)
+        superview.addConstraints([sender.horConstraint!  , sender.verConstraint!, widConstr, heiConstr])
+
+    }
+    
+    
+    func addAnimation(sender: CubesView, superview: UIView) {
+        // constr
+        sender.horConstraint?.constant = CGFloat(arc4random_uniform(UInt32(superview.frame.size.width - superview.frame.size.width * 0.2)))
+        sender.verConstraint?.constant = superview.frame.size.height * 0.1
         
         
+        // animation
         
-        let timer = Timer.scheduledTimer(withTimeInterval: 0.0, repeats: false) {_ in
+        //DispatchQueue.main.async {  // Dispatch not work correct
             
-            UIView.animate(withDuration: 5.0, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 0.1, options: UIViewAnimationOptions.allowUserInteraction,
-            animations: {
-                verConstr.constant = superview.frame.size.height
-                superview.layoutIfNeeded()
-            }, completion: {_ in
+        
+        
+        let timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: false) {_ in
+            
+            UIView.animate(withDuration: 5.0, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 0.1,
+                           options: [UIViewAnimationOptions.allowUserInteraction, UIViewAnimationOptions.curveLinear],
+                           animations: {
+                            
+                            sender.verConstraint?.constant = superview.frame.size.height
+                            
+                            superview.layoutIfNeeded()
+                            
+            }, completion: { finished in
                 
-             
-                // SKSpriteMode
-                
-                   
-                if sender.frame.origin.y >= 300 {
-                    verConstr.constant = 100
+                if finished {
+                    self.addAnimation(sender: sender, superview: superview)
+                    self.countOfCubDidDown += 1
+                    self.countOfCubDidDownLabel.text = String(self.countOfCubDidDown)
                 }
-                    
-                
-                
-                //self.arrayOfCubesItem(sender: sender, superview: self.view)
-                //verConstr.constant = superview.frame.size.height * 0.1
-            })
-            
-            
-            
-            /*
-            UIView.animate(withDuration: 5.0, delay: 0.0, options: UIViewAnimationOptions.allowUserInteraction, animations: {
-                verConstr.constant = superview.frame.size.height
-                superview.layoutIfNeeded()
-            }, completion: {_ in
-                
-                
                 
             })
-            */
+            
         }
         RunLoop.current.add(timer, forMode: .commonModes)
         
-        
     }
-    
-    func arrayOfCubesItem(sender: UIView, superview: UIView) {
-        
-        let horConstr = NSLayoutConstraint(item: sender,
-                                           attribute: .left,
-                                           relatedBy: .equal ,
-                                           toItem: superview,
-                                           attribute: .left,
-                                           multiplier: 1.0,
-                                           constant: CGFloat(arc4random_uniform(UInt32(superview.frame.size.width)-UInt32(superview.frame.size.height * 0.1))))
-        let verConstr = NSLayoutConstraint(item: sender,
-                                           attribute: .top,
-                                           relatedBy: .equal,
-                                           toItem: superview,
-                                           attribute: .top,
-                                           multiplier: 1.0,
-                                           constant: superview.frame.size.height * 0.1)
-        let widConstr = NSLayoutConstraint(item: sender, attribute: .width, relatedBy: .equal, toItem: superview, attribute: .width, multiplier: 0.2, constant: 0.0)
-        let heiConstr = NSLayoutConstraint(item: sender, attribute: .height, relatedBy: .equal, toItem: superview, attribute: .height, multiplier: 0.1, constant: 0.0)
-        superview.addConstraints([horConstr, verConstr, widConstr, heiConstr])
-        
-    }
-
-
 }
 
